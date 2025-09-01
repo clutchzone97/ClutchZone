6427f7ac8be317f122ae8b18704f13cb28ea2d4f
clutchzone <clutchzone97@gmail.com>
2025-09-01 15:44:06 +0300

    fix vite base path for vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',   // ✨ ده أهم تعديل
   server: {
     port: 3000,
     proxy: {
